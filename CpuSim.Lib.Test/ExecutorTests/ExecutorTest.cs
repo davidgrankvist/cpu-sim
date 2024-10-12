7b675ec9d27e6bf2d1307e2a8bfa2e5fcad2a967
David Grankvist <grankvist.david@gmail.com>
2024-10-12 14:52:49 +0200

    add executor tests

---
+++ CpuSim.Lib.Test/ExecutorTests/ExecutorTest.cs
@@ -0,0 +1,49 @@
+﻿using CpuSim.Lib.Simulation;
+using CpuSim.Lib.Simulation.Commands;
+
+namespace CpuSim.Lib.Test.ExecutorTests
+{
+    [TestClass]
+    public class ExecutorTest
+    {
+        private Executor executor;
+        private CpuState cpuState;
+        private int numRegisters;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            numRegisters = 2;
+            cpuState = new CpuState(numRegisters);
+            executor = new Executor(cpuState);
+        }
+
+        [TestMethod]
+        public void ShouldCombineArithmetic()
+        {
+            var (program, resultRegister, expectedResult) = ExecutorTestDataHelper.CreateCombinedArithmeticProgram();
+
+            RunProgram(program);
+
+            Assert.AreEqual(expectedResult, cpuState.GetRegister(resultRegister));
+        }
+
+        [TestMethod]
+        public void ShouldLoadIncStore()
+        {
+            var (program, resultAddress, expectedResult) = ExecutorTestDataHelper.CreateLoadIncStoreProgram();
+
+            RunProgram(program);
+
+            Assert.AreEqual(expectedResult, cpuState.GetMemory(resultAddress));
+        }
+
+        private void RunProgram(IEnumerable<ICpuCommand> commands)
+        {
+            foreach (var command in commands)
+            {
+                executor.Execute(command);
+            }
+        }
+    }
+}
---
+++ CpuSim.Lib.Test/ExecutorTests/ExecutorTestDataHelper.cs
@@ -0,0 +1,47 @@
+﻿using CpuSim.Lib.Simulation.Commands;
+using CpuSim.Lib.Simulation.Commands.Arithmetic;
+
+namespace CpuSim.Lib.Test.ExecutorTests
+{
+    internal static class ExecutorTestDataHelper
+    {
+        public static (IEnumerable<ICpuCommand> Program, int ResultRegister, int ExpectedResult) CreateCombinedArithmeticProgram()
+        {
+            var expectedResult = 5 + 3 * 6 / 2 - 2;
+            var resultRegister = 0;
+
+            var r1 = resultRegister;
+            var r2 = 1;
+            var program = new List<ICpuCommand>()
+            {
+                new LoadCommand(r1, 3),
+                new LoadCommand(r2, 6),
+                new MultiplyCommand(r1, r2),
+                new LoadCommand(r2, 2),
+                new DivideCommand(r1, r2),
+                new LoadCommand(r2, 5),
+                new AddCommand(r1, r2),
+                new LoadCommand(r2, 2),
+                new SubtractCommand(r1, r2),
+            };
+
+            return (program, r1, expectedResult);
+        }
+
+        public static (IEnumerable<ICpuCommand> Program, int ResultAddress, int ExpectedResult) CreateLoadIncStoreProgram()
+        {
+            var expectedResult = 5;
+            var resultAddress = 0x1337;
+
+            var r = 0;
+            var program = new List<ICpuCommand>()
+            {
+                new LoadCommand(r, 4),
+                new IncrementCommand(r),
+                new StoreCommand(r, resultAddress),
+            };
+
+            return (program, resultAddress, expectedResult);
+        }
+    }
+}
